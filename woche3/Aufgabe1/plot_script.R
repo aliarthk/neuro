5fbc33867ff790fc05a652ec9f62cc1ec1ef4517
marasteiger <mara@marpe-steiger.de>
2019-01-22 20:08:11 +0100

    plot1

---
+++ woche3/Aufgabe1/plot_script.R
@@ -0,0 +1,7 @@
+setwd("/Users/marasteiger/Documents/Uni/Neuro/Praktikum/tag3/Aufgabe1")
+
+a1 <- read.table("1_2.txt", header = T)
+
+plot(log(a1[,1]),a1[,2], xlab = "log(Kalium-Konzentration)", ylab = "" ,col = "blue")
+lines(log(a1[,1]),a1[,3],type="p", col = "red")
+legend("topleft", legend=c("Gleichgewichtspotential Kalium", "Membranpotential"), col = c("blue","red"), pch = c(1,1))
